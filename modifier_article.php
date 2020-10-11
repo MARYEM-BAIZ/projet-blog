398ed7d78a637e5ed5ee5339ba821311b704f7e3
maryem <baiz.maryem2018@gmail.com>
2020-10-11 05:39:34 +0200

    commit 22

--- modifier_article.php
+++ modifier_article.php
@@ -214,8 +214,13 @@ elseif ($_SESSION['roleutilisateur']==2) { ?>
      <p class="h1 mb-4"> Modufication du article </p>
       <form class="text-center   " action="#" method="post" enctype="multipart/form-data">
          <input class="form-control mb-4" type="text" name="titre_article" value=" <?php echo $_GET['titre_article']  ?>">
-         <input class="form-control mb-4" type="text" name="contenu_article" value=" <?php echo $_GET['contenu_article']  ?>">
+   
         
+         <div class="form-group shadow-textarea">
+  <!-- <label for="exampleFormControlTextarea6">Shadow and placeholder</label> -->
+  <textarea name="contenu_article"  class="form-control z-depth-1" id="exampleFormControlTextarea6" rows="3" placeholder="Contenu d'article"> <?php echo $_GET['contenu_article']  ?></textarea>
+</div>
+
          <div class="input-group">
   
   <div class="mb-4  custom-file">
@@ -270,5 +275,10 @@ elseif ($_SESSION['roleutilisateur']==2) { ?>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+<script src="ckeditor5-build-classic/ckeditor.js"></script>
+<script>
+  ClassicEditor.create(document.getElementById("exampleFormControlTextarea6"))
+ 
+</script>
 </body>
 </html>
